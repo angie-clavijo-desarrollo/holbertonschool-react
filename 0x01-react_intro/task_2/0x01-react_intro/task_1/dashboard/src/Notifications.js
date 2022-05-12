6beabada509384e8347755cbf6ea39f948ecd385
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-05-11 22:19:53 -0500

    correction

--- 0x01-react_intro/task_2/0x01-react_intro/task_1/dashboard/src/Notifications.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "/Notifications.css";
-
-export default function Notification() {
-  <div className="Notifications">
-    <p>Here is the list of notifications</p>
-  </div>;
-}
